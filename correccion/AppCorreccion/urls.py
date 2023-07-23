99706569367098bca8b2cb8561985d3ec068efba
Mariana Cohen <marucohenisaac@gmail.com>
2023-07-23 09:29:19 -0300

    Segundo commit rama correccion_2: creación de login, logout y registro

--- correccion/AppCorreccion/urls.py
+++ correccion/AppCorreccion/urls.py
@@ -1,6 +1,22 @@
 from django.urls import path
 from AppCorreccion import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-        path('',views.inicio, name='inicio')
+        path('',views.inicio, name='inicio'),
+        
+        path('curso/', views.curso, name='curso'),
+        path('mostrar_cursos/', views.mostrar_cursos, name='mostrar_cursos'),
+        path('buscar_curso/', views.buscar_curso, name="buscar_curso"),
+        path('eliminar-curso/<id>/', views.eliminar_curso, name="eliminar_curso"),
+        path('curso_detalle/<pk>/', views.CursoDetalle.as_view(), name="curso_detalle"),
+
+        path('login/', views.login_request, name='login'),
+        path('register/', views.register, name='register'),
+        path('logout/', views.Logout.as_view(), name='logout'),
+        
 ]
+
+#
+#
--- correccion/AppCorreccion/views.py
+++ correccion/AppCorreccion/views.py
@@ -1,8 +1,85 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Curso
+from .forms import BuscaCursoForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+from AppCorreccion import forms
+from django.contrib.auth.views import LogoutView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 def inicio(request): 
     return render(request, "AppCorreccion/inicio.html")
+
+def curso(request):
+      
+      if request.method == 'POST':
+        curso =  Curso(nombre=request.POST['nombre'])
+        curso.save()
+        return render(request, "AppCorreccion/curso_bien.html")
+      return render(request,"AppCorreccion/curso.html")
+
+def buscar_curso(request):
+    if request.method == "POST":
+        busca_curso = BuscaCursoForm(request.POST)
+
+        if busca_curso.is_valid():
+            info = busca_curso.cleaned_data
+            cursos = Curso.objects.filter(nombre=info["curso"])
+            return render(request, "AppCorreccion/lista_curso.html", {"cursos": cursos})
+    else:
+        busca_curso = BuscaCursoForm()
+        return render(request, "AppCorreccion/buscar_curso.html", {"miFormulario": busca_curso})
+
+def mostrar_cursos(request):
+    cursos = Curso.objects.all()
+    contexto= {"cursos":cursos} 
+    return render(request, "AppCorreccion/mostrar_cursos.html",contexto)
+
+def eliminar_curso(request, id):
+    curso = Curso.objects.get(id=id)
+    curso.delete()
+    curso = Curso.objects.all()
+    contexto = {"cursos": curso}
+    return render (request, "AppCorreccion/mostrar_cursos.html", contexto)
+
+class CursoDetalle(DetailView):
+    model = Curso
+    template_name = "AppEntrega/curso_detalle.html"
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = forms.RegistroUsuarioForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            return render(request, 'AppCorreccion/registro.html', {'form':form})
+    form = forms.RegistroUsuarioForm()
+    return render(request, 'AppCorreccion/registro.html', {'form':form})
+
+def login_request(request):
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid():
+            usuario = form.cleaned_data.get('username')
+            contrasenia = form.cleaned_data.get('password')
+            user = authenticate(username= usuario, password=contrasenia)
+
+            if user is not None:
+                login(request, user)
+                return redirect("inicio")
+            else:
+                return render(request, "AppCorreccion/login.html", {"mensaje": "Datos incorrectos"})
+
+    form = AuthenticationForm()
+    return render(request, "AppCorreccion/login.html", {"form": form})
+
+class Logout(LogoutView):
+    template_name = 'AppCorreccion/logout.html'
 
 #def clases(request):
 
